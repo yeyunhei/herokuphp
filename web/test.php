d1a6df31f01840b77f29c53514f2978011a4057d
yeyunhei <20011806@myrp.edu.sg>
2022-07-07 12:15:38 +0800

    move test.php

--- web/test.php
+++ web/test.php
@@ -1,5 +1,10 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
+<title>Title</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="Put your description here.">
+
 </head>
 <body>
   Hello World!
